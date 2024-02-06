adbb10da523e8de0c009959c57672bfbe575f69c
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-02-06 17:03:08 +0000

    Finished off the examples of booleans

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args){
         boolean newBool = true;//this is true
         boolean oldBool = false;//this is not
-
+        System.out.println(20 > 5);//this will print true
+        System.out.println(5 > 20);//this will print false
+        //and that's all of booleans really, variables and expressions. True expressions return true, false expressions will return false.
     }
 }
